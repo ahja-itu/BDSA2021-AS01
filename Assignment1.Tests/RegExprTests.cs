ecc26173ff72bb304facde4912bdc31c21254f83
Andreas Wachs Hjalager <ahja@itu.dk>
2021-09-10 14:01:22 +0200

    Implement method and tests for Resolution

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -35,5 +35,37 @@ namespace Assignment1.Tests
 
             Assert.Equal(new string[] {"the", "quick", "brown", "fox"}, result);
         }
+
+        [Fact]
+        public void Resolution_given_list_of_resolutions_give_formatted_resolutions()
+        {
+            var resolutions = "1920x1080 " +
+                               "1024x768, 800x600, 640x480 " +
+                               "320x200, 320x240, 800x600 " +
+                               "1280x960";
+
+            var results = RegExpr.Resolution(resolutions);
+
+            var expected = new int[][] {
+                new int[] {1920, 1080},
+                new int[] {1024, 768},
+                new int[] {800, 600},
+                new int[] {640, 480},
+                new int[] {320, 200},
+                new int[] {320, 240},
+                new int[] {800, 600},
+                new int[] {1280, 960}};
+
+            int counter = 0;
+            foreach(var result in results)
+            {
+                var (width, height) = result;
+
+                Assert.Equal(expected[counter][0], width);
+                Assert.Equal(expected[counter][1], height);
+                counter++;
+            }
+            
+        }
     }
 }
--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -24,7 +24,14 @@ namespace Assignment1
 
         public static IEnumerable<(int width, int height)> Resolution(string resolutions)
         {
-            throw new NotImplementedException();
+            var regex = new Regex(@"(?'width'\d+)x(?'height'\d+)\s*");
+
+            foreach(Match match in regex.Matches(resolutions))
+            {
+                int width = int.Parse(match.Groups["width"].ToString());
+                int height = int.Parse(match.Groups["height"].ToString());
+                yield return (width, height);
+            }
         }
 
         public static IEnumerable<string> InnerText(string html, string tag)
